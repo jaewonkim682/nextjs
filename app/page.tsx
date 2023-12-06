d2b30517201ef95f92db409112ce3318a3a426b0
jaewonkim682 <56789211+jaewonkim682@users.noreply.github.com>
2023-12-06 14:02:29 +0000

    feat: intermediate commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,42 @@
-import Image from "next/image";
+"use client";
+import { getSystemErrorName } from "util";
+import AddTask from "./AddTask";
+import React, { useState } from "react";
 
-export default function Home() {
-  return <div>Todo lists, ultimate version</div>;
-}
+const NameInputButton = () => {
+  const [name, setName] = useState("");
+  const [namesList, setNamesList] = useState([]);
+
+  const handleInputChage = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    if (name.trim() !== "") {
+      setNamesList((prevNames) => [...prevNames, name]);
+      setName("");
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Type your name"
+        value={name}
+        onChange={handleInputChage}
+      />
+      <button onClick={handleButtonClick}>Add Task</button>
+      <div>
+        <h2>Task List:</h2>
+        <ul>
+          {namesList.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NameInputButton;
