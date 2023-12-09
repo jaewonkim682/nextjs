dd9de124dbd3028f829dd07dacf35cd8f806db77
jaewonkim682 <56789211+jaewonkim682@users.noreply.github.com>
2023-12-09 14:29:23 +0000

    first working example

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,67 @@
 "use client";
-import { getSystemErrorName } from "util";
-import AddTask from "./AddTask";
-import React, { useState } from "react";
+
+import React, { ChangeEvent, MouseEvent, useState } from "react";
 
 const NameInputButton = () => {
   const [name, setName] = useState("");
-  const [namesList, setNamesList] = useState([]);
+  const [namesList, setNamesList] = useState<string[]>([]);
 
-  const handleInputChage = (event) => {
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
   };
 
   const handleButtonClick = () => {
     if (name.trim() !== "") {
-      setNamesList((prevNames) => [...prevNames, name]);
       setName("");
+      setNamesList((prevNames) => [...prevNames, name]);
     }
+  };
+
+  const deleteButtonClick = () => {
+    setNamesList(namesList.slice(0, namesList.length - 1));
+    // const list = namesList;
+    // console.log(list);
+    // setNamesList(list.slice(0, list.length - 1));
+  };
+
+  const taskDeleteButton = (index: number) => {
+    setNamesList((taskLsts) => taskLsts.filter((_, i) => i !== index));
   };
 
   return (
     <div>
+      <button
+        className="absolute top-0 left-96 underline bg-sky-400"
+        onClick={handleButtonClick}
+      >
+        Add Task
+      </button>
+      <button
+        className="absolute top-0 left-0 underline bg-red-400"
+        onClick={deleteButtonClick}
+      >
+        Delete Task
+      </button>
       <input
+        className="absolute top-0 right-96 bg-slate-300"
         type="text"
         placeholder="Type your name"
         value={name}
-        onChange={handleInputChage}
+        onChange={handleInputChange}
       />
-      <button onClick={handleButtonClick}>Add Task</button>
       <div>
-        <h2>Task List:</h2>
-        <ul>
+        <h2 className="absolute top-10 left-96 bg-emerald-400"> Task List: </h2>
+        <ul className="absolute top-16 left-96 text-lg">
           {namesList.map((item, index) => (
-            <li key={index}>{item}</li>
+            <li key={index}>
+              {item}
+              <button
+                className="underline bg-red-400"
+                onClick={() => taskDeleteButton(index)}
+              >
+                Delete Task
+              </button>
+            </li>
           ))}
         </ul>
       </div>
